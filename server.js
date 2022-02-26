2aca4a069e6666a3e892fcc9eb8c7fc03359f6bf
thaiscmky <thaiscmky@github.com>
2022-02-25 21:35:02 -0500

    Sets up initial server file

---
+++ server.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022, Thaís Cailet, @thaiscmky. All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE.txt file for terms.
+ */
+
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
